a2f0784dc379f531c7004cfb602ced0a9b70dddc
denamo <artstarxd@outlook.com>
2022-02-10 16:49:05 +0300

    init

---
+++ Sharp10/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Sharp10
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("выберите задание для проверки");
+            
+            int choose = Convert.ToInt32(Console.ReadLine());
+
+            switch (choose)
+            {
+                case 1:
+                {
+                    int input = Convert.ToInt32(Console.ReadLine());
+                    string secondInput = Console.ReadLine();
+                    Console.WriteLine(Tasks.GetChar(2, "faggot"));
+                    Console.WriteLine(Tasks.GetChar(input, secondInput));
+                    Console.WriteLine(Tasks.GetChar(0, "HOHOL"));
+                    break;
+                }
+                case 2:
+                    Console.WriteLine(Tasks.CheckLetters("maggot", "tiger"));
+                    break;
+                
+                case 3:
+                    Console.WriteLine(Tasks.MakeSomeMagic("MEGAPRIKOL", 1));
+                    Console.WriteLine(Tasks.MakeSomeMagic("MEGAPRIKOL", 2));
+                    break;
+                
+                case 7:
+                    Console.WriteLine(Tasks.DoSomething("вирус", 1));
+                    break;
+                
+                case 8:
+                    Console.WriteLine(Tasks.DoSomething("курсор", 2));
+                    break;
+                
+                case 9:
+                    Console.WriteLine(Tasks.DoSomething("пробел", 3));
+                    break;
+                
+                case 10:
+                    Console.WriteLine(Tasks.DoSomething("строка", 4));
+                    break;
+            }
+        }
+    }
+}
---
+++ Sharp10/Tasks.cs
@@ -0,0 +1,57 @@
+﻿namespace Sharp10
+{
+    public static class Tasks
+    {
+        public static char GetChar(int pos, string line)
+        {
+            return line[^1] == line[0] ? 'd' : line[1] == line[3] ? 'c' : line[pos];
+        }
+
+        public static bool CheckLetters(string fLine, string sLine)
+        {
+            return fLine[^1] == sLine[0];
+        }
+
+        public static string MakeSomeMagic(string line, int task)
+        {
+            return task switch
+            {
+                1 => line[1] + "" + line[3],
+                2 => line[1] + "" + line[2] + "" + line[3],
+                _ => "what the fuck bro"
+            };
+        }
+
+        public static string DoSomething(string line, int task)
+        {
+            switch (task)
+            {
+                //TODO: отвратительный говнокод, изменить когда-нибудь
+                case 1:
+                    line = line.Replace('в', 'ф');
+                    line = line.Replace('и', 'о');
+                    line = line.Replace('р', 'к');
+
+                    return line;
+                case 2:
+                    line = line.Replace('к', 'т');
+                    line = line.Replace('у', 'а');
+                    line = line.Replace('р', 'н');
+                    line = line.Replace('с', 'ц');
+
+                    return line;
+                case 3:
+                    line = line.Replace('б', 'д');
+
+                    return line;
+                case 4:
+                    line = line.Replace('к', 'ф');
+
+                    return line;
+                
+                default:
+                    return "хуета";
+            }
+        }
+    }
+}
